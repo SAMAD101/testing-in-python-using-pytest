b17911a22ea1190b25816db5efcbfad83a9cbf28
Sam <asamadans@gmail.com>
2023-12-19 01:07:55 +0530

    Functions tests

---
+++ tests/test_some_functions.py
@@ -0,0 +1,16 @@
+import pytest
+import src.some_functions as some_functions
+
+def test_add():
+    assert some_functions.add(1, 2) == 3
+
+def test_divide():
+    assert some_functions.divide(2, 1) == 2
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        some_functions.divide(1, 0)
+    assert str(e.value) == 'division by zero'
+
+def test_add_strings():
+    assert some_functions.add('a', 'b') == 'ab'
