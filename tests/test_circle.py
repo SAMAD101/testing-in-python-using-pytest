72f261233c63805f157962c60e6bb11b4c14668a
Sam <asamadans@gmail.com>
2023-12-19 20:06:32 +0530

    some refactor; parametrize

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,15 +1,20 @@
 import pytest
 from src.circle import Circle
 
+
 @pytest.fixture
 def circle():
     return Circle(3)
 
-def test_area(circle):
-    assert circle.area() == 28.26
+
+@pytest.mark.parametrize("radius, expected", [(1, 3.14), (2, 12.56), (3, 28.26)])
+def test_area(radius, expected):
+    assert Circle(radius).area() == expected
+
 
 def test_circumference(circle):
     assert circle.circumference() == 18.84
+
 
 def test_repr(circle):
     assert repr(circle) == "Circle(3)"
--- tests/test_some_functions.py
+++ tests/test_some_functions.py
@@ -2,16 +2,20 @@ import pytest
 import time
 import src.some_functions as some_functions
 
+
 def test_add():
     assert some_functions.add(1, 2) == 3
 
+
 def test_divide():
     assert some_functions.divide(2, 1) == 2
+
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         some_functions.divide(1, 0)
     assert str(e.value) == 'division by zero'
+
 
 def test_add_strings():
     assert some_functions.add('a', 'b') == 'ab'
@@ -22,9 +26,11 @@ def test_add_slow():
     time.sleep(1)
     assert some_functions.add(1, 2) == 3
 
+
 @pytest.mark.skip(reason="Not implemented yet")
 def test_add():
     assert some_functions.add(1, 2) == 3
+
 
 @pytest.mark.xfail(reason="Can't divide by zero")
 def test_divide_by_zero():
